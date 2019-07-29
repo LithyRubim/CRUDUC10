889959c4411ba67ca437dfc1d3d43e4edc67dc71
AlexWilliam <alex.william@outlook.com>
2019-07-28 23:04:27 -0300

    banco de dados

--- biblioteca.sql
+++ biblioteca.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : SERVER(MYSQL5)
+ Source Server         : LOCALHOST(MySQL5)
  Source Server Type    : MySQL
- Source Server Version : 100138
+ Source Server Version : 50562
  Source Host           : localhost:3307
  Source Schema         : biblioteca
 
  Target Server Type    : MySQL
- Target Server Version : 100138
+ Target Server Version : 50562
  File Encoding         : 65001
 
- Date: 26/07/2019 17:29:54
+ Date: 28/07/2019 23:02:33
 */
 
 SET NAMES utf8mb4;
@@ -27,13 +27,14 @@ CREATE TABLE `livros`  (
   `genero` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `autor` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `editora` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `edicao` int(3) NULL DEFAULT NULL,
   `dataLancamento` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of livros
 -- ----------------------------
-INSERT INTO `livros` VALUES (1, 'Teste de Livro', 'Ação', 'Alex', 'Maia', '05/08/2019');
+INSERT INTO `livros` VALUES (3, 'Silmarilo', 'Aventura, Ação', 'J.R.R. Tolkien', 'Abril', 3, '05/01/1990');
 
 SET FOREIGN_KEY_CHECKS = 1;
